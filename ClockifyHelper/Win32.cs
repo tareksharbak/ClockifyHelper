3a8932c7a56c640b2c38b8cb14c4ec8dd43b7dde
Tarek Sharbak <tarek.sharbak@diflexmo.be>
2021-10-28 16:42:17 +0200

    cleanup

--- ClockifyHelper/Win32.cs
+++ ClockifyHelper/Win32.cs
@@ -3,14 +3,10 @@ using System.Runtime.InteropServices;
 
 namespace ClockifyHelper
 {
-	internal class Win32
-	{
-		[DllImport("user32.dll", CharSet = CharSet.Auto,
-				   CallingConvention = CallingConvention.StdCall)]
-		internal static extern bool GetLastInputInfo(out Win32LastInputInfo plii);
-
-		private Win32()
-		{
-		}
+    internal class Win32
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto,
+                   CallingConvention = CallingConvention.StdCall)]
+        internal static extern bool GetLastInputInfo(out Win32LastInputInfo plii);
 	}
 }
